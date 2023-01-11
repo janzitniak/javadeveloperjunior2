d8dafdfbe8ca32275478d17a7c0ee18b0a8b1517
Ján Žitniak on GitHub.com <54016618+janzitniak@users.noreply.github.com>
2023-01-11 16:10:51 +0100

    Adding notice about adding favorite car

--- src/basicexamples/ArraysExample_8.java
+++ src/basicexamples/ArraysExample_8.java
@@ -2,7 +2,7 @@ package basicexamples;
 
 public class ArraysExample_8 {
     public static void main(String[] args) {
-        String cars[] = {"Honda", "Mazda", "Audi"};
+        String cars[] = {"Honda", "Mazda", "Audi"}; // Add your favorite car vendor
         for (int i=0; i < cars.length; i++) {
             System.out.println(cars[i]);
         }
