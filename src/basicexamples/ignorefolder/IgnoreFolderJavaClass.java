6c0926ef4192afc4ff1cca5de8a59c9778910001
janzitniak <jan.zitniak@gmail.com>
2023-01-11 15:05:22 +0100

    Testing ignoring folder in git.

---
+++ src/basicexamples/ignorefolder/IgnoreFolderJavaClass.java
@@ -0,0 +1,7 @@
+package basicexamples.ignorefolder;
+
+public class IgnoreFolderJavaClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World from IgnoreFolderJavaClass");
+    }
+}
