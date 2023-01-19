a87e538a4d89edabc451928b7da4c17135943da7
janzitniak <jan.zitniak@gmail.com>
2023-01-19 15:26:27 +0100

    Adding several jUnit tests into inner class. Using @Nested and @DisplayName annotations.

--- src/basicexamples/exercises/Rectangle.java
+++ src/basicexamples/exercises/Rectangle.java
@@ -1,10 +1,13 @@
 package basicexamples.exercises;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 /* Úloha č. 1
 Vytvorte triedu Rectangle, v ktorej budú 2 statické metódy. Prvá na výpočet obvodu obdĺžnika (getPerimeter), druhá na výpočet jeho obsahu (getArea).
@@ -29,33 +32,70 @@ public class Rectangle {
     public static void main(String[] args) {
         double a;
         double b;
+        String odpoved;
 
-        Scanner scn = new Scanner(System.in);
+        while (true) {
+            Scanner scn = new Scanner(System.in); // inicializacia "konzoly" pre pouzivatelsky vstup
 
-        System.out.println("Zadaj hodnotu strany a: ");
-        a = scn.nextDouble();
+            System.out.printf("%nZadaj:");
+            System.out.println("(s) pre vstup hodnôt na výpočet obvodu a obsahu obdĺžnika");
+            System.out.println("(q) pre ukončenie programu");
 
-        System.out.println("Zadaj hodnotu strany b: ");
-        b = scn.nextDouble();
+            odpoved = scn.nextLine(); // premenna odpoved bude obsahovat pismeno (moznost), ktore zadal pouzivatel
 
-        double obvod = getPerimeter(a, b);
-        double obsah = getArea(a, b);
-        System.out.println("Obvod kruhu je: " + obvod);
-        System.out.println("Obsah kruhu je:" + obsah);
+            if (odpoved.equals("s")) {
+                System.out.println("Zadaj hodnotu strany a: ");
+                a = scn.nextDouble();
+
+                System.out.println("Zadaj hodnotu strany b: ");
+                b = scn.nextDouble();
+
+                double obvod = Rectangle.getPerimeter(a, b);
+                double obsah = Rectangle.getArea(a, b);
+                System.out.println("Obvod kruhu je: " + obvod);
+                System.out.println("Obsah kruhu je:" + obsah);
+            } else if (odpoved.equals("q")) {
+                System.out.println("Koniec programu");
+                break;
+            } else {
+                System.out.println("Zadali ste nesprávnu možnosť! Zopakujte výber.");
+            }
+        }
+
     }
 
+/*
     @Test
     public void testGetPerimeter() {
-        assertEquals(10, getPerimeter(2, 3));
-        assertEquals(13, getPerimeter(2.5, 4));
+        assertEquals(10, getPerimeter(2,3));
+        assertEquals(13, getPerimeter(2.5,4));
+        assertEquals(0, getPerimeter(0,0));
     }
 
     @Test
     public void testGetArea() {
         assertEquals(6, getArea(2, 3));
-        assertEquals(10, getArea(2.5, 4));
-        assertEquals(5, getArea(1, 5));
+        assertEquals(10, getArea(2, 5));
     }
+*/
+
+    @Nested
+    @DisplayName("Tests for my method")
+    class MyMethodUseCases {
+        @Test
+        public void testGetPerimeter() {
+            assertEquals(10, getPerimeter(2, 3));
+            assertEquals(13, getPerimeter(2.5, 4));
+        }
+
+        @Test
+        public void testGetArea() {
+            assertEquals(6, getArea(2, 3));
+            assertEquals(10, getArea(2.5, 4));
+            assertEquals(5, getArea(1, 5));
+        }
+    }
+
 
 }
 
