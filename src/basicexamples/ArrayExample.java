fccd9cd38ebe4504915857227b9b71160cfbae1d
janzitniak <jan.zitniak@gmail.com>
2023-01-22 16:29:25 +0100

    ExcelBasicFormulas.java with all methods for SUM, AVERAGE, COUNT, MAX and MIN. jUnit tests for getSum and getAverage methods.

--- src/basicexamples/ArrayExample.java
+++ src/basicexamples/ArrayExample.java
@@ -1,5 +1,8 @@
 package basicexamples;
 
+
+import java.util.Arrays;
+
 public class ArrayExample {
     public static void main(String[] args) {
         int [] values = {8, 7, 3, 999};
@@ -10,10 +13,43 @@ public class ArrayExample {
         // chceme vypisat vsetky hodnoty pola values EFEKTIVNE!!!
         // System.out.println(values);
 
-        for (int i = 0; i <=3; i++) {
+        for (int i = 0; i <= 3; i++) {
             System.out.println(values[i]); // vypiseme obsah pola prostrednictvom kombinacie nazovPola[i]
         }
 
+        System.out.println("-----------------------------------");
+
+        int[] array = new int[10]; // vytvorime prazdne pole, ktore bude obsahovat 10 prvkov
+
+        // naplnime kazdy prvok pola nahodnou hodnotou od 1 do 100
+        array[0] = ((int) (Math.random() * 100 + 1));
+        array[1] = ((int) (Math.random() * 100 + 1));
+        array[2] = ((int) (Math.random() * 100 + 1));
+        array[3] = ((int) (Math.random() * 100 + 1));
+        array[4] = ((int) (Math.random() * 100 + 1));
+        array[5] = ((int) (Math.random() * 100 + 1));
+        array[6] = ((int) (Math.random() * 100 + 1));
+        array[7] = ((int) (Math.random() * 100 + 1));
+        array[8] = ((int) (Math.random() * 100 + 1));
+        array[9] = ((int) (Math.random() * 100 + 1));
+
+        // tu vypiseme kazdy prvok pola
+        System.out.println(array[0] + " " + array[1] + " " + array[2] + " " + array[3]
+                + " " + array[4] + " " + array[5] + " " + array[6] + " " + array[7] + " "
+                + array[8] + " " + array[9]);
+
+
+        Arrays.sort(array); // zoradi pole od najmensieho po najvacsie
+
+        // System.out.println(array); // zobrazit takymto sposobom nepojde
+
+        System.out.println("Zoradené údaje (od najmenšieho po najväčšie)");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.println("\nNajväčšie číslo je: " + array[array.length-1]);
+        System.out.println("Najmenšie číslo je: " + array[0]);
 
     }
 }
--- src/basicexamples/exercises/ExcelBasicFormulas.java
+++ src/basicexamples/exercises/ExcelBasicFormulas.java
@@ -1,30 +1,96 @@
 package basicexamples.exercises;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /*
 Vytvorte triedu s názvom ExcelBasicFormulas, v ktorej budú 2 základné funkcie SUM a AVERAGE, ktoré budú počítať súčet (getSum), resp. priemer hodnôt (getAverage).
         Vstup hodnôt bude cez premenné, napr. value1 a value2.
 */
 public class ExcelBasicFormulas {
-    public static void main(String[] args) {
-        int[] values = {10, 20, 100};
-        int sum = 0;
 
-        System.out.println("Počet prvkov v poli values je: " + values.length);
-
+    public static double getSum(double [] values) {
+        // tu musi byt logika vypoctu
+        double sum = 0;
         for (int i = 0; i < values.length; i++) { // cyklus s pevnym poctom opakovani, i nadobuda hodnoty od 0 do 3
-            //System.out.println(values[i]);
             sum = sum + values[i]; // Java interpretuje tento riadok najprv tak, ze zoberie alebo vypocita to, co je napravo od =
-            // V 1. iteracii bude sum obsahovat hodnotu 10, pretoze 0 + 10
-            // V 2. iteracii bude sum obsahovat hodnotu 30, pretoze 10 + 20
-            // V 3. iteracii bude sum obsahovat hodnotu 60, pretoze 30 + 30
-            // V 4. iteracii bude sum obsahovat hodnotu 100, pretoze 60 + 40
-
         }
-        // V premennej sum zostanem posledny sucet, cize 100
-        System.out.println("SUM " + sum);
+        return sum;
+    }
 
-        double average = sum / values.length;
-        System.out.println("AVERAGE " + average);
+    public static double getAverage(double sum, int count) {
+        double average = sum / count;
+        return average;
+    }
+
+    public static double getAverageFromArray(double [] values) {
+        double sum = getSum(values);
+        int count = values.length;
+        double average = sum / count;
+        return average;
+    }
+
+    public static double getAverageFromArrayWithoutSumMethod(double [] values) {
+        // tato metoda vypocita aritmeticky priemer, vstupom je pole s hodnotami [] values
+        double sum = 0;
+        for (int i = 0; i < values.length; i++) { // cyklus s pevnym poctom opakovani, i nadobuda hodnoty od 0 do 3
+            sum = sum + values[i]; // Java interpretuje tento riadok najprv tak, ze zoberie alebo vypocita to, co je napravo od =
+        }
+        int count = values.length;
+        double average = sum / count;
+        return average;
+    }
+
+    public static int getCount(double [] values) {
+        int count = values.length;
+        return count;
+    }
+
+    public static double getMax(double [] values) {
+        Arrays.sort(values);
+        double max = values[values.length-1];
+        return max;
+    }
+
+    public static double getMin (double [] values) {
+        Arrays.sort(values); // pole values je zoradene
+        double min = values[0];
+        return min;
+    }
+
+    public static void main(String[] args) {
+        // Chceme vypisat vysledky nasledujucich Excel funkcii v poradi: SUM, AVERAGE, COUNT, MAX, MIN
+        double[] values = {1, 2, 3}; // naplname pole s nazvom values hodnotami
+
+        // V premennej sum zostanem posledny sucet
+        System.out.println("SUM " + getSum(values));
+        System.out.println("AVERAGE " + getAverageFromArrayWithoutSumMethod(values));
+        System.out.println("COUNT " + getCount(values));
+        System.out.println("MAX " + getMax(values));
+        System.out.println("MIN " + getMin(values));
 
     }
+
+
+    @Test
+    public void testGetSum() {
+        double[] values = {1, 2, 3}; // naplname pole s nazvom values hodnotami
+        assertEquals(6, getSum(values));
+
+        double[] values2 = {3,8,10,100};
+        assertEquals(121, getSum(values2));
+    }
+
+    @Test
+    public void testGetAverage() {
+        double[] values = {1, 2, 3}; // naplname pole s nazvom values hodnotami
+        assertEquals(2, getAverageFromArrayWithoutSumMethod(values));
+
+        double[] values2 = {1,1,1};
+        assertEquals(1, getAverageFromArrayWithoutSumMethod(values2));
+    }
+
 }
