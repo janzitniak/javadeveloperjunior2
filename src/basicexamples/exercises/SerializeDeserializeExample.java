7901138be95dc08255158b0278e8a961ed9fb78d
janzitniak <jan.zitniak@gmail.com>
2023-02-05 14:06:10 +0100

    Adding SerializeDeserializeExample.java example to show how to serialize (save to file) and deserialize (load from file) the object. KnihaSpravne.java need implements Serializable interface.

---
+++ src/basicexamples/exercises/SerializeDeserializeExample.java
@@ -0,0 +1,35 @@
+package basicexamples.exercises;
+
+import basicexamples.exercises.model.KnihaSpravne;
+
+import java.io.*;
+
+public class SerializeDeserializeExample {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        KnihaSpravne knihaSpravne1 = new KnihaSpravne();
+        knihaSpravne1.setNazov("Motýľ");
+        knihaSpravne1.setAutor("Anna Schonn");
+        knihaSpravne1.setZaner("Ženský román");
+
+        FileOutputStream fileOutputStream
+                = new FileOutputStream("yourfile.txt");
+        ObjectOutputStream objectOutputStream
+                = new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(knihaSpravne1);
+        objectOutputStream.flush();
+        objectOutputStream.close();
+
+
+        FileInputStream fileInputStream
+                = new FileInputStream("yourfile.txt");
+        ObjectInputStream objectInputStream
+                = new ObjectInputStream(fileInputStream);
+
+        KnihaSpravne knihaSpravneZoSuboru = (KnihaSpravne) objectInputStream.readObject();
+        objectInputStream.close();
+
+
+        System.out.println(knihaSpravneZoSuboru);
+
+    }
+}
--- src/basicexamples/exercises/model/KnihaSpravne.java
+++ src/basicexamples/exercises/model/KnihaSpravne.java
@@ -1,7 +1,10 @@
 package basicexamples.exercises.model;
 
-public class KnihaSpravne {
+import java.io.Serializable;
+
+public class KnihaSpravne implements Serializable {
     // fields, cize premenne triedy
+    // private static final long serialVersionUID = 1L; // odporuca sa pouziva, Intellij IDEA si serialVersionUID generuje sama, moze vsak nastat konflikt
     private String obsah;
     private String zaner;
     private String nazov;
